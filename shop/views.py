75c389cbc040d9e483ddbf459ed15330a57cb948
Dmytro Saienko <saenko26051978@gmail.com>
2023-07-22 20:20:00 +0300

    Added user authentication

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+
 
 def home(request):
     return render(request, 'shop/home.html')
+
+
+def signupuser(request):
+    form = UserCreationForm()
+    if request.method == 'GET':
+        return render(request, 'shop/signupuser.html', {'form': form})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(),
+                                                                'error': 'That username has been already taken. '
+                                                                         'Choose another one'})
+
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(),
+                                                            'error': 'Passwords did not match'})
